a755ec4a3ed3d30a3f09dfc1231773b3d90eac1e
tobitoast4 <tobi-30@gmx.de>
2023-08-27 01:42:25 +0200

    Added some comments

--- app/simulation/person.py
+++ app/simulation/person.py
@@ -9,6 +9,9 @@ DICT_KEY_AMOUNT_MATCHES = "amount_matches"
 
 
 def get_opposite_sex(sex):
+    """Gets the opposite sex.
+       If male (1) is given, female is returned (2). If female (2) is given, male (1) is returned.
+    """
     if sex == SEX_MALE:
         return SEX_FEMALE
     elif sex == SEX_FEMALE:
@@ -18,12 +21,20 @@ def get_opposite_sex(sex):
 
 
 class User:
-    def __init__(self, sex, attractivity, max_amount_of_likes):
+    def __init__(self, sex, attractiveness, max_amount_of_likes):
+        """Creates a new instance of User. This class represents a user / profile in a dating app.
+
+        Args:
+            sex:                    The sex of the user.
+            attractiveness:         The attractiveness. Should be a value from 0 tp 100.
+            max_amount_of_likes:    The amount of profiles this user can like.
+                                    If None, they have endless likes.
+        """
         self.sex = sex
-        self.attractivity = attractivity
+        self.attractiveness = attractiveness
         self.amount_of_likes_available = max_amount_of_likes
-        self.likes_given = []
-        self.likes_received = []
+        self.likes_given = []       # this will contain references to other users that have been liked
+        self.likes_received = []    # this will contain references to other users that gave this one a like
         self.amount_matches = 0
 
     def like_profile(self, user):
@@ -42,6 +53,8 @@ class User:
                     self.amount_matches += 1
 
     def to_dict(self):
+        """Returns this user in a dictionary. Can be used for creating a dataframe.
+        """
         return {
             DICT_KEY_OBJECT_ID: id(self),
             DICT_KEY_SEX: self.sex,
--- app/simulation/simulation.py
+++ app/simulation/simulation.py
@@ -13,18 +13,25 @@ def like_current_profile_random(probability_rate):
 
 class Simulation:
     def __init__(self, amount_men, amount_women, amount_women_seen_by_one_man, amount_men_seen_by_one_woman,
-                 max_amount_of_likes_of_one_man, max_amount_of_likes_of_one_woman, formula_for_men_attractivity,
-                 formula_for_women_attractivity):
+                 max_amount_of_likes_of_one_man, max_amount_of_likes_of_one_woman, formula_for_men_attractiveness,
+                 formula_for_women_attractiveness):
         """Creates a new instance of Simulation.
 
-        :param amount_men:
-        :param amount_women:
-        :param amount_women_seen_by_one_man:
-        :param amount_men_seen_by_one_woman:
-        :param max_amount_of_likes_of_one_man:
-        :param max_amount_of_likes_of_one_woman:
-        :param formula_for_men_attractivity: Should be parsed already.
-        :param formula_for_women_attractivity: Should be parsed already.
+        Args:
+            amount_men:                         The amount of men there will be in the simulation.
+            amount_women:                       The amount of women there will be in the simulation.
+            amount_women_seen_by_one_man:       The amount of women a man will see in the simulation.
+                                                E.g.: If set to 100, one man can swipe 100 profiles left or right.
+            amount_men_seen_by_one_woman:       The amount of men a woman will see in the simulation.
+            max_amount_of_likes_of_one_man:     The maximum amount of like one man has.
+                                                If set to None, he will have unlimited likes.
+            max_amount_of_likes_of_one_woman:   The maximum amount of like one woman has.
+                                                If set to None, she will have unlimited likes.
+            formula_for_men_attractiveness:     The formula to determine the attractiveness for men. The attractiveness
+                                                for each single man will follow this formula. Should be parsed already.
+                                                -> Means x^6.14 should already be written in pythonic way (as x**6.14).
+                                                -> See ./utils.py -> parse_equation()
+            formula_for_women_attractiveness:   The formula to determine the attractiveness for women.
         """
         self.amount_men = amount_men
         self.amount_women = amount_women
@@ -32,8 +39,8 @@ class Simulation:
         self.amount_men_seen_by_one_woman = amount_men_seen_by_one_woman
         self.max_amount_of_likes_of_one_man = max_amount_of_likes_of_one_man
         self.max_amount_of_likes_of_one_woman = max_amount_of_likes_of_one_woman
-        self.formula_for_men_attractivity = formula_for_men_attractivity
-        self.formula_for_women_attractivity = formula_for_women_attractivity
+        self.formula_for_men_attractivity = formula_for_men_attractiveness
+        self.formula_for_women_attractivity = formula_for_women_attractiveness
         # progress_to_reach is set to (AMOUNT_MEN + AMOUNT_WOMEN) * 3 as all users will be iterated 3 times:
         # once for creating, once every user will swipe and once for calculating the matches
         self.progress = Progress((self.amount_men + self.amount_women) * 3)
@@ -45,6 +52,9 @@ class Simulation:
         self.latest_error = None
 
     def get_random_sample_of_users_by_sex(self, sex):
+        """Gets a random sample of users of one sex. The size of the sample depends on
+           self.amount_men_seen_by_one_woman / self.amount_women_seen_by_one_man.
+        """
         if sex == SEX_MALE:
             return random.sample(self.list_of_men, self.amount_men_seen_by_one_woman)
         elif sex == SEX_FEMALE:
@@ -53,6 +63,8 @@ class Simulation:
             raise ValueError
 
     def get_users_by_sex(self, sex):
+        """Gets all the users of one sex in the simulation.
+        """
         if sex == SEX_MALE:
             return self.list_of_men
         elif sex == SEX_FEMALE:
@@ -61,12 +73,15 @@ class Simulation:
             raise ValueError
 
     def run_like_process_for_sex(self, sex):
+        """Runs the like process for all users of one sex by iterating all these users and
+           letting each of them swipe through the given amount of profiles.
+        """
         opposite_sex = get_opposite_sex(sex)
         for current_user in self.get_users_by_sex(sex):
             self.progress.increase_progress()
             subset_of_other_users = self.get_random_sample_of_users_by_sex(opposite_sex)
             for current_profile in subset_of_other_users:
-                like_probability = current_profile.attractivity
+                like_probability = current_profile.attractiveness
                 if like_current_profile_random(like_probability):
                     current_user.like_profile(current_profile)
 
@@ -97,8 +112,15 @@ class Simulation:
         return median_user.amount_matches
 
     def run_sim(self, time_sleep):
-        """
-        :param time_sleep: Can be used to show the progress bar for a short amount of time.
+        """Runs the simulation. That includes creating all male and female users with their attractiveness level,
+           swiping the profiles and calculating the matches. In between the status of the progress of the simulation
+           will also be updated.
+
+        Args:
+            time_sleep: Can be used to show the progress bar for a short amount of time.
+
+        Raises:
+            SyntaxError / Exception.
         """
         try:
             self.progress.current_progress_status_text = PROGRESS_STATE_TEXT_STATUS_1
@@ -149,11 +171,13 @@ PROGRESS_STATE_TEXT_STATUS_5 = "Simulation succeeded"
 
 class Progress:
     def __init__(self, progress_to_reach):
-        """
-        progress_to_reach: Amount of processing cycles to be done. Simple example:
-                          Download of files from website. Downloading a file could be seen as one processing
-                          cycle. self.progress_to_reach should be set to this amount of files. After a
-                          processing cycle is done, current_progress should be increased.
+        """Creates a new instance of Progress.
+
+        Args:
+            progress_to_reach:    Amount of processing cycles to be done. Simple example:
+                                  Download of files from website. Downloading a file could be seen as one processing
+                                  cycle. self.progress_to_reach should be set to this amount of files. After a
+                                  processing cycle is done, current_progress should be increased.
         """
         self.progress_to_reach = progress_to_reach
         self.current_progress = 0
