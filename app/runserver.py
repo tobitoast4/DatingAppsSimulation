344bd4d0e35d5b9aa512ed529c4abe2b5ec1e220
tobitoast4 <tobi-30@gmx.de>
2023-09-15 01:43:26 +0200

    Update runserver.py

--- app/runserver.py
+++ app/runserver.py
@@ -1,6 +1,4 @@
-from webapp.app import app
+from webapp.app import server
 
-# if __name__ == "__main__":
-#     server.run("127.0.0.1", port=80, debug=False, threaded=True, processes=1)
-
-app.run(debug=True)
+if __name__ == "__main__":
+    server.run("127.0.0.1", port=80, debug=False, threaded=True, processes=1)
